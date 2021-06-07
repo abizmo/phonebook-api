ea369b46f8b5108a71be5fd7b9df1126effb7c9b
Abián Izquierdo <abizmo@gmail.com>
2021-06-07 17:00:38 +0100

    Frontend added

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ let persons = [
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static('build'));
+
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(
   morgan(
